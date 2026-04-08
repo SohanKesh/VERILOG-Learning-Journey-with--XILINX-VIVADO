66e24947c4b358d78c5cdec495773ae0dd2cc384
SohanKesh <sohankmurthy366@gmail.com>
2026-04-08 19:17:13 +0200

    Add files via upload

---
+++ tb_dff (2).v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.04.2026 13:57:19
+// Design Name: 
+// Module Name: td_dff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module td_dff(
+
+    );
+endmodule
